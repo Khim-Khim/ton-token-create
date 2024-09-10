634964f5d94b85908317044dad9960e78739a0f4
khiemht <khiemht@bap.jp>
2024-09-10 11:13:12 +0700

    chero: fix config

--- src/consts.ts
+++ src/consts.ts
@@ -14,7 +14,7 @@ const APP_DISPLAY_NAME = "TON MINTER";
 const JETTON_DEPLOYER_CONTRACTS_GITHUB = "https://github.com/ton-blockchain/minter-contract";
 
 const EXAMPLE_ADDRESS =
-  getNetwork(new URLSearchParams(window.location.search)) === "testnet"
+  getNetwork() === "testnet"
     ? "EQBP4L9h4272Z0j_w9PE2tjHhi8OwkrRbTmatKszMyseis05"
     : "EQD-LkpmPTHhPW68cNfc7B83NcfE9JyGegXzAT8LetpQSRSm";
 
